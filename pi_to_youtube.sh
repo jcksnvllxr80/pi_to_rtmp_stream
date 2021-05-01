49dd0eac81744761c0883b553be46c8f2fd5a5f0
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-01 17:14:18 -0400

    add service

---
+++ pi_to_youtube.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+raspivid -o - -t 0 -fps 30 -b 6000000 -w 1640 -h 1232 | ffmpeg -re -ar 44100 -ac 1 -acodec pcm_s16le -f s16le -ac 1 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/kv9b-4axc-8vdv-kjss-3j4h
