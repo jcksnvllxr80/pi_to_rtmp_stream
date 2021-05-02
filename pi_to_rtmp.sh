b825470b6b8b5d0cfd6be9e2c8d599fc19b9da75
Aaron Watkins <ac.watkins80@gmail.com>
2021-05-01 21:44:48 -0400

    revert last test commit

--- pi_to_rtmp.sh
+++ pi_to_rtmp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-# sdfgsdfg raspivid -o - -t 0 -fps 30 -b 6000000 -w 1640 -h 1232 | ffmpeg -re -ar 44100 -ac 1 -acodec pcm_s16le -f s16le -ac 1 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/kv9b-4axc-8vdv-kjss-3j4h  # Youtube
+# raspivid -o - -t 0 -fps 30 -b 6000000 -w 1640 -h 1232 | ffmpeg -re -ar 44100 -ac 1 -acodec pcm_s16le -f s16le -ac 1 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/kv9b-4axc-8vdv-kjss-3j4h  # Youtube
 raspivid -o - -t 0 -fps 30 -b 6000000 -w 1640 -h 1232 | ffmpeg -re -ar 44100 -ac 1 -acodec pcm_s16le -f s16le -ac 1 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://video1.earthcam.com/myearthcam/4257=21e37a3b790bae8f336692441c15c7c4  # EarthCam
