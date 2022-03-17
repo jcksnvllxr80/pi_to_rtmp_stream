9f4970c2a512296aff053b90d07b93508e9d5cb7
Aaron Watkins <ac.watkins80@gmail.com>
2022-03-17 01:11:14 -0400

    add ability to record and stream

--- pi_to_rtmp.sh
+++ pi_to_rtmp.sh
@@ -1,3 +1,8 @@
-#!/bin/bash 
-# raspivid -o - -t 0 -fps 30 -b 6000000 -w 1640 -h 1232 | ffmpeg -re -ar 44100 -ac 1 -acodec pcm_s16le -f s16le -ac 1 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/kv9b-4axc-8vdv-kjss-3j4h  # Youtube
-raspivid -o - -t 0 -fps 30 -b 6000000 -w 1640 -h 1232 | ffmpeg -re -ar 44100 -ac 1 -acodec pcm_s16le -f s16le -ac 1 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict normal -f flv rtmp://video1.earthcam.com/myearthcam/4257=21e37a3b790bae8f336692441c15c7c4  # EarthCam
+#!/bin/bash
+# raspivid -o - -t 0 -fps 30 -b 6000000 -w 1640 -h 1232 | ffmpeg -re -ar 44100 -ac 1 -acodec pcm_s16le -f s16le -ac 1 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/<youtube-secret-key>  # Youtube
+
+# this next one is what works without also saving to file
+# raspivid -o - -t 0 -fps 30 -b 6000000 -w 1640 -h 1232 | ffmpeg -re -ar 44100 -ac 1 -acodec pcm_s16le -f s16le -ac 1 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict normal -f flv rtmp://video1.earthcam.com/myearthcam/4257=<earthcam-secret-key>  # EarthCam
+
+# comment when video is done being recorded
+raspivid -o - -t 0 -fps 30 -b 6000000 -w 1640 -h 1232 | tee /home/pi/$(date +"%Y-%m-%dT%H-%M-%SZ")-babybirds.h264 | ffmpeg -re -ar 44100 -ac 1 -acodec pcm_s16le -f s16le -ac 1 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict normal -f flv rtmp://video1.earthcam.com/myearthcam/4257=<earthcam-secret-key>  # EarthCam
